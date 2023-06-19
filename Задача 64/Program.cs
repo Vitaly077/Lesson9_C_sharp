173237eac304aa57f13ab64ad0febda70842f96a
Vitaly <kentanohin478@gmail.com>
2023-06-19 18:04:15 +0300

    Lesson 9

---
+++ Задача 64/Program.cs
@@ -0,0 +1,30 @@
+﻿/*Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*/
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите N: ");
+        int n = int.Parse(Console.ReadLine());
+
+        PrintNumbers(n);
+    }
+
+    static void PrintNumbers(int num)
+    {
+        if (num < 1)
+        {
+            return;
+        }
+
+        Console.Write("{0} ", num);
+        PrintNumbers(num - 1);
+    }
+}
+
---
+++ Задача 66/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите M: ");
+        int m = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите N: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int sum = 0;
+        for (int i = m; i <= n; i++)
+        {
+            sum += i;
+        }
+
+        Console.WriteLine("Сумма натуральных элементов от {0} до {1} равна {2}", m, n, sum);
+    }
+}
